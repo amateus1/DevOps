0862253ef69d1627c359a6e92fad2815ea6198af
Alveiro Hernan Mateus <alveiro.h.mateus@avanade.com>
2020-04-08 17:36:45 +0800

    Updated with SCM and Repo

---
+++ src/test/java/com/example/coedevops2/Coedevops2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coedevops2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coedevops2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
