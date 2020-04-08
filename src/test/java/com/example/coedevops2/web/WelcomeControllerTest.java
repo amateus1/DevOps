2ed5165294d24908cf6879497642304d5f86fb93
Alveiro Hernan Mateus <alveiro.h.mateus@avanade.com>
2020-04-09 03:17:53 +0800

    added missing dependencies

--- src/test/java/com/example/coedevops2/web/WelcomeControllerTest.java
+++ src/test/java/com/example/coedevops2/web/WelcomeControllerTest.java
@@ -1,20 +1,20 @@
 package com.example.coedevops2.web;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -23,16 +23,19 @@ import org.springframework.web.context.WebApplicationContext;
 public class WelcomeControllerTest {
 
 	private MockMvc mockMvc;
-	
+
 	@Autowired
 	private WebApplicationContext context;
-	
+
 	@Before
-	public void setup() throws Exception {
+	public void setUp() throws Exception {
 		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
 	}
+
 	@Test
 	public void testWelcome() throws Exception {
-		mockMvc.perform(get("/")).andExpect(status().isOk());
+		mockMvc.perform(get("/")).andExpect(status().isOk())
+				.andExpect(model().attribute("course", containsString("template")));
 	}
+
 }
