6368ccb5aeff5cc573a30d500a5328b0ac27701f
amateus1 <al@mateusit.com>
2020-04-09 16:32:29 +0800

    Update ServletInitializer.java

--- src/main/java/com/example/coedevops2/ServletInitializer.java
+++ src/main/java/com/example/coedevops2/ServletInitializer.java
@@ -2,7 +2,6 @@ package com.example.coedevops2;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.web.servlet.support;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
